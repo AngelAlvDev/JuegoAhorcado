bfcfc6f9c0644c5adeb7dde36df5405955815e81
AngelAlvAlumnCESCristoRey <angelalv.alumn@cescristorey.com>
2020-11-14 18:37:03 +0100

    Agregador boton general de salir

---
+++ JuegoAhorcado/JuegoAhorcado/AhorcadoView.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace JuegoAhorcado
+{
+    partial class AhorcadoView
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AhorcadoView));
+            this.exitPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.exitPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitPictureBox
+            // 
+            this.exitPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("exitPictureBox.Image")));
+            this.exitPictureBox.Location = new System.Drawing.Point(919, 544);
+            this.exitPictureBox.Name = "exitPictureBox";
+            this.exitPictureBox.Size = new System.Drawing.Size(90, 82);
+            this.exitPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.exitPictureBox.TabIndex = 0;
+            this.exitPictureBox.TabStop = false;
+            this.exitPictureBox.Click += new System.EventHandler(this.exitPictureBox_Click);
+            // 
+            // AhorcadoView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 656);
+            this.Controls.Add(this.exitPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "AhorcadoView";
+            this.Text = "Ahorcado V 1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.exitPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox exitPictureBox;
+    }
+}
+
---
+++ JuegoAhorcado/JuegoAhorcado/AhorcadoView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoAhorcado
+{
+    public partial class AhorcadoView : Form
+    {
+        public AhorcadoView()
+        {
+            InitializeComponent();
+        }
+
+        private void exitPictureBox_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("¿Seguro que desea salir de la aplicación ?","Cerrando el juego",MessageBoxButtons.YesNo);
+            if(result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
